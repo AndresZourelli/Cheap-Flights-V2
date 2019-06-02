2ba284f73cb85415f7290932603f78adda0f2254
Andres <azourelli@gmail.com>
2019-06-02 00:17:55 -0500

    fixed auto complete

--- client/src/Components/Autocomplete/Autocomplete.js
+++ client/src/Components/Autocomplete/Autocomplete.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import airports from '../../Data/airports.json';
 import { getFlights, setFlightForm } from '../../actions/apiActions.js';
 import { connect } from 'react-redux';
+import './Autocomplete.css';
 class Autocomplete extends Component {
 	constructor(props) {
 		super(props);
@@ -41,6 +42,7 @@ class Autocomplete extends Component {
 		}
 		let results = search(event.target.value, airports.airports);
 		if (!event.target.value) {
+			form.fly_from = '';
 			this.setState({ form: form, displayResultDeparture: [] });
 		} else {
 			form['fly_from'] = event.target.value;
@@ -56,6 +58,7 @@ class Autocomplete extends Component {
 
 		let results = search(event.target.value, airports.airports);
 		if (!event.target.value) {
+			form.fly_to = '';
 			this.setState({ form: form, displayResultDestination: [] });
 		} else {
 			form['fly_to'] = event.target.value;
@@ -85,45 +88,62 @@ class Autocomplete extends Component {
 		let indexResultsDepartures = this.state.displayResultDeparture.map((data) => (
 			<div
 				key={data.ICAO}
-				className="p-3 border-right border-bottom border-dark border-left"
+				className="p-3 border-right border-bottom border-dark border-left dropdown"
 				onClick={() => this.onClickDeparture(data.name, data.IATA)}>
-				<h4>Name: {data.name}</h4>
+				<h4>
+					<strong>City:</strong> {data.city}  <strong>Name: </strong>
+					{data.name} <strong>Airport Code:</strong> {data.IATA}
+				</h4>
 			</div>
 		));
 		let indexResultsDestination = this.state.displayResultDestination.map((data) => (
 			<div
 				key={data.ICAO}
-				className="p-3 border-right border-bottom border-dark border-left"
+				className="p-3 border-right border-bottom border-dark border-left dropdown"
 				onClick={() => this.onClickDestination(data.name, data.IATA)}>
-				<h4>Name: {data.name}</h4>
+				<h4>
+					<strong>City:</strong> {data.city}  <strong>Name: </strong>
+					{data.name} <strong>Airport Code:</strong> {data.IATA}
+				</h4>
 			</div>
 		));
+		console.log(this.state.displayResultDestination === []);
 		return (
 			<div className="">
-				<div className="input-group m-3">
+				<div className="input-group p-3">
 					<input
 						type="text"
 						value={this.state.form.fly_from}
 						className="form-control"
-						placeholder="Departure"
+						placeholder="Fly From"
 						onChange={(e) => this.onChangeDeparture(e)}
 						aria-label="Username"
 						aria-describedby="basic-addon1"
 					/>
+					{this.state.displayResultDeparture.length === 0 ? null : (
+						<div style={{ width: '100%', position: 'relative' }}>
+							<div className="dropdown_list">{indexResultsDepartures}</div>
+						</div>
+					)}
 				</div>
-				{indexResultsDepartures}
-				<div className="input-group m-3">
+
+				<div className="input-group p-3">
 					<input
 						type="text"
 						value={this.state.form.fly_to}
 						className="form-control"
-						placeholder="Destination"
+						placeholder="Fly to"
 						onChange={(e) => this.onChangeDestination(e)}
 						aria-label="Username"
 						aria-describedby="basic-addon1"
 					/>
+					{this.state.displayResultDestination.length === 0 ? null : (
+						<div style={{ width: '100%', position: 'relative' }}>
+							<div className="dropdown_list">{indexResultsDestination}</div>
+						</div>
+					)}
 				</div>
-				{indexResultsDestination}
+
 				<button onClick={this.onSubmit}>SUBMIT</button>
 			</div>
 		);
